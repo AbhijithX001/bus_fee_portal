adc08532d3ce5d59ae43ef4f7778bb05d1638520
Abhijith P <abhijithp@Abhijiths-MacBook-Air.local>
2025-12-28 12:10:57 +0530

    Add superuser creation to build script

--- build.sh
+++ build.sh
@@ -4,3 +4,13 @@ set -o errexit
 pip3 install -r requirements.txt
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
+
+python3 manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123');
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
